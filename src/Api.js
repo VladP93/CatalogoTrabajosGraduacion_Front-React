f2fcc5780c374739cf54d4adcc49d07f905b33be
Vladimir Paniagua <vladimirpaniagua@gmail.com>
2021-01-20 22:56:53 -0600

    server api changed

--- src/Api.js
+++ src/Api.js
@@ -1,5 +1,5 @@
 const url = {
-  getUrl: "http://daw2020-001-site1.dtempurl.com/api/",
+  getUrl: "https://daw2020actalogoapi.azurewebsites.net/api/",
   //getUrl: "https://localhost:44341/api/",
 };
 
